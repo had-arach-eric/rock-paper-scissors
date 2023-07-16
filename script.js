fef63892a2b949562dc0109bba5d669bd58b916c
Eric Had Arach <hadarachericj@gmail.com>
2023-07-15 22:39:39 -0300

    Make a function 'playRound'.

--- script.js
+++ script.js
@@ -2,17 +2,48 @@ function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
   switch(randomNumber) {
     case 1:
-      return "Rock";
+      return "rock";
       break;
     case 2:
-      return "Paper";
+      return "paper";
       break;
     case 3:
-      return "Scissors";
+      return "scissors";
       break;
   }
 }
 
-/*for (let i = 0; i < 100; i++) {
-  const value = getComputerChoice();
-}*/
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      return "You tied the round. Both elections were rock";
+    } else if (computerSelection === "paper") {
+        return "You lost the round! Paper beats rock";
+    } else {
+        return "You won the round! Rock beats scissors";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You won the round! Paper beats rock";
+    } else if (computerSelection === "paper") {
+        return "You tied the round. Both elections were paper";
+    } else {
+        return "You lost the round! Scissors beats paper";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "You lost the round! Rock beats scissors";
+    } else if (computerSelection === "paper") {
+        return "You won the round! Scissors beats paper";
+    } else {
+        return "You tied the round. Both elections were scissors";
+    }
+  } else {
+      return "Your choice was wrong";
+  }
+  
+  
+}
+
+
