7093c5af0ad338d1aabeb81362365fddf2797f8f
Eric Had Arach <hadarachericj@gmail.com>
2023-07-20 15:57:21 -0300

    Synthesise code with resetUI function.

--- script.js
+++ script.js
@@ -13,65 +13,76 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection, points, playerPointsUI, computerPointsUI, roundMessage) {
+function playRound(playerSelection, computerSelection, points, playerPointsUI, computerPointsUI, roundMessageUI) {
   if (playerSelection === "rock") {
     if (computerSelection === "rock") {
-      roundMessage.textContent = "You tied the round. Both elections were rock";
+      roundMessageUI.textContent = "You tied the round. Both elections were rock";
     }
     else if (computerSelection === "paper") {
       points.computerPoints++;
       computerPointsUI.textContent = points.computerPoints; 
-      roundMessage.textContent = "You lost the round! Paper beats rock";
+      roundMessageUI.textContent = "You lost the round! Paper beats rock";
     }
     else {
       points.playerPoints++;
       playerPointsUI.textContent = points.playerPoints; 
-      roundMessage.textContent = "You won the round! Rock beats scissors";
+      roundMessageUI.textContent = "You won the round! Rock beats scissors";
     }
   } 
   else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
       points.playerPoints++;
       playerPointsUI.textContent = points.playerPoints; 
-      roundMessage.textContent = "You won the round! Paper beats rock";
+      roundMessageUI.textContent = "You won the round! Paper beats rock";
     }
     else if (computerSelection === "paper") {
-      roundMessage.textContent = "You tied the round. Both elections were paper";
+      roundMessageUI.textContent = "You tied the round. Both elections were paper";
     }
     else {
       points.computerPoints++;
       computerPointsUI.textContent = points.computerPoints;
-      roundMessage.textContent = "You lost the round! Scissors beats paper";
+      roundMessageUI.textContent = "You lost the round! Scissors beats paper";
     }
   } 
   else {
     if (computerSelection === "rock") {
       points.computerPoints++;
       computerPointsUI.textContent = points.computerPoints;
-      roundMessage.textContent = "You lost the round! Rock beats scissors";
+      roundMessageUI.textContent = "You lost the round! Rock beats scissors";
     }
     else if (computerSelection === "paper") {
       points.playerPoints++;
       playerPointsUI.textContent = points.playerPoints; 
-      roundMessage.textContent = "You won the round! Scissors beats paper";
+      roundMessageUI.textContent = "You won the round! Scissors beats paper";
     }
     else {
-      roundMessage.textContent = "You tied the round. Both elections were scissors";
+      roundMessageUI.textContent = "You tied the round. Both elections were scissors";
     }
   } 
 }
 
 
-function getFinalReport(playerPoints, computerPoints, finalMessage) {
+function getFinalReport(playerPoints, computerPoints, finalMessageUI) {
   if (playerPoints > computerPoints) {
-    finalMessage.textContent = "YOU WON THE GAME";
-    finalMessage.style.color = "green";
+    finalMessageUI.textContent = "YOU WON THE GAME";
+    finalMessageUI.style.color = "green";
   }
   else if (playerPoints < computerPoints) {
-    finalMessage.textContent = "YOU LOST THE GAME";
-    finalMessage.style.color = "red";
+    finalMessageUI.textContent = "YOU LOST THE GAME";
+    finalMessageUI.style.color = "red";
   }
 }
+
+function resetUI(points, playerPointsUI, computerPointsUI, roundMessageUI, finalMessageUI) {
+  points.playerPoints = 0;
+  points.computerPoints = 0;
+  playerPointsUI.textContent = "0";
+  computerPointsUI.textContent = "0";
+  roundMessageUI.textContent = "";
+  finalMessageUI.textContent = "";
+}
+
+
 
 
 function game() {
@@ -81,31 +92,24 @@ function game() {
   };
   let playerSelection = "";
   
-  // DOM node references
   const playButtonsUI = document.querySelectorAll(".play-buttons");
-  let messageUI = document.querySelector("#round-message");
-  const finalMessage = document.querySelector("#final-message");
-  const reset = document.querySelector(".reset-button");
   const playerPointsUI = document.querySelector("#player-points");
   const computerPointsUI = document.querySelector("#computer-points");
-  const roundMessage = document.querySelector("#round-message");
-  reset.addEventListener("click", () => {
-    points.playerPoints = 0;
-    points.computerPoints = 0;
-    messageUI.textContent = "";
-    finalMessage.textContent = "";
-    playerPointsUI.textContent = "0";
-    computerPointsUI.textContent = "0";
-    roundMessage.textContent = "";
-  })
+  const roundMessageUI = document.querySelector("#round-message");
+  const finalMessageUI = document.querySelector("#final-message");
+  const resetButtonUI = document.querySelector(".reset-button");
+
+  resetButtonUI.addEventListener("click", () => {
+    resetUI(points, playerPointsUI, computerPointsUI, roundMessageUI, finalMessageUI);
+  });
   
   for (let i = 0; i < playButtonsUI.length; i++) {
     playButtonsUI[i].addEventListener("pointerdown", e => {
       playButtonsUI[i].style.border = "4px solid red";
       playerSelection = e.target.getAttribute("id");
-      playRound(playerSelection, getComputerChoice(), points, playerPointsUI, computerPointsUI, roundMessage);
+      playRound(playerSelection, getComputerChoice(), points, playerPointsUI, computerPointsUI, roundMessageUI);
       if (points.playerPoints === 5 || points.computerPoints === 5) {
-        getFinalReport(points.playerPoints, points.computerPoints, finalMessage);
+        getFinalReport(points.playerPoints, points.computerPoints, finalMessageUI);
       }
     });
   }
@@ -114,14 +118,6 @@ function game() {
       playButtonsUI[i].style.border = "";
     });
   }
-
-
-  
-  
 }
-
-
-
-
 
 game();
