4a5d5295539daacb17c225edbc5555ca1fefc13c
Eric Had Arach <hadarachericj@gmail.com>
2023-07-20 15:11:15 -0300

    Finalise logic that makes it work.

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection, points) {
   const playerPointsUI = document.querySelector("#player-points");
   const computerPointsUI = document.querySelector("#computer-points");
-  const message = document.querySelector("#message");
+  const message = document.querySelector("#round-message");
+  console.log(playerSelection);
+  console.log(computerSelection);
 
   if (playerSelection === "rock") {
     if (computerSelection === "rock") {
@@ -65,34 +67,35 @@ function playRound(playerSelection, computerSelection, points) {
   } 
 }
 
-/*
+
 function getFinalReport(playerPoints, computerPoints) {
+  const message = document.querySelector("#final-message");
   if (playerPoints > computerPoints) {
-    return `You won the game: the final result is: Player(${playerPoints}) - Computer(${computerPoints})`;
+    message.textContent = "YOU WON THE GAME";
   }
   else if (playerPoints < computerPoints) {
-    return `You lost the game: the final result is: Player(${playerPoints}) - Computer(${computerPoints})`;
-  }
-  else {
-    return `You tied the game: the final result is: Player(${playerPoints}) - Computer(${computerPoints})`;
+    message.textContent = "YOU LOST THE GAME";
   }
 }
-*/
+
 
 function game() {
   const points = {
     playerPoints: 0,
     computerPoints: 0,
   };
-
   let playerSelection = "";
-  let computerSelection = getComputerChoice();
   let playButtonsUI = document.querySelectorAll(".play-buttons");
-  let messageUI = document.querySelector("#message");
+  let messageUI = document.querySelector("#round-message");
   
   for (let i = 0; i < playButtonsUI.length; i++) {
     playButtonsUI[i].addEventListener("pointerdown", e => {
       playButtonsUI[i].style.border = "4px solid red";
+      playerSelection = e.target.getAttribute("id");
+      playRound(playerSelection, getComputerChoice(), points);
+      if (points.playerPoints === 5 || points.computerPoints === 5) {
+        getFinalReport(points.playerPoints, points.computerPoints);
+      }
     });
   }
   for (let i = 0; i < playButtonsUI.length; i++) {
@@ -100,35 +103,13 @@ function game() {
       playButtonsUI[i].style.border = "";
     });
   }
-  for (let i = 0; i < playButtonsUI.length; i++) {
-    playButtonsUI[i].addEventListener("click", e => {
-      playerSelection = e.target.getAttribute("id");
-    });
-  }
 
-  /*
-  for (button of playButtonsUI) {
-    button.addEventListener("mousedown", e => {
-      button.style.border = "4px solid red";
-    });
-  }
-  for (button of playButtonsUI) {
-    button.addEventListener("mouseup", e => {
-      button.style.border = "";
-    });
-  for (button of playButtonsUI) {
-    button.addEventListener("click", e => {
-      playerSelection = e.target.getAttribute("id");
-    });
-  }
-  */
   
-  }
-
-  /*
-  playRound(playerSelection, computerSelection, points);
+  
 }
-*/
+  
+
+
 
 
 
